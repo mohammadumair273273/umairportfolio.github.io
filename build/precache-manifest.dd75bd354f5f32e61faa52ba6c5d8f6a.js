4886940473af60a728b6f0a70ae2eab898d6b832
mohammadumair273273 <72144368+mohammadumair273273@users.noreply.github.com>
2020-10-01 18:38:08 +0500

    Add files via upload

---
+++ build/precache-manifest.dd75bd354f5f32e61faa52ba6c5d8f6a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86c109e473a7582de7ec669ff5854c85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9d156e79405c649fbc8",
+    "url": "/static/css/main.d58084d2.chunk.css"
+  },
+  {
+    "revision": "2bf58c66db46c026ddbe",
+    "url": "/static/js/2.b6ca5f6c.chunk.js"
+  },
+  {
+    "revision": "f9d156e79405c649fbc8",
+    "url": "/static/js/main.a97b35bd.chunk.js"
+  },
+  {
+    "revision": "9e0dbbbecde63d8dbba4",
+    "url": "/static/js/runtime-main.b79f7dce.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "fbcc4986900cb5eb045225172e497a3b",
+    "url": "/static/media/airbnbLogo.fbcc4986.jpg"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "35b222700e0a9d68cbd1c946cb432575",
+    "url": "/static/media/facebookLogo.35b22270.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "a586475763504025499893ea7a646b3d",
+    "url": "/static/media/systems.a5864757.jpg"
+  }
+]);
